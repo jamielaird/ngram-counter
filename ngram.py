91e50f97c6a193be5b1fd434041242426e3f2265
Jamie Laird <jamie.laird@hobsons.com>
2017-06-20 10:15:42 +0100

    Can now loop through items in a CSV

--- ngram.py
+++ ngram.py
@@ -14,12 +14,15 @@ c = 3
 def find_ngrams(input_list, n):
     return zip(*[input_list[i:] for i in range(n)])
 
+print('----------------------------------------------')
+
 # Read from csv file
 with open ('sample-email.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         # Add contents of the body field to full_text
         full_text = row['body']
+        email = row['email']
 
         # Convert all text to lowercase
         full_text = str.lower(full_text)
@@ -36,13 +39,19 @@ with open ('sample-email.csv') as csvfile:
         # Tally occurrences of ngrams for this piece of text
         cnt = Counter(ngram_list)
 
+        # Print the email address
+        print (email)
+
         # Print the number of items in the list
         print ('There are '+str(sum(cnt.values()))+' '+str(n)+'-grams in the file.')
 
         # Print the full list of n-grams
-        print ('The full set of '+str(n)+'-grams is: '),(Counter(cnt))
+        print ('The full set of '+str(n)+'-grams is:')
+        print (Counter(cnt))
 
         # Print the most common phrases
         print ('The '+str(c)+' most common '+str(n)+'-grams are: ',(Counter(cnt).most_common(c)))
+
+        print('----------------------------------------------')
 
         continue
